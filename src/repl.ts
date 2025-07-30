0285bf05315b336953f83d5e02e3f29b9b3cbdec
josephus <otejerejosephus@gmail.com>
2025-07-30 20:23:59 +0100

    added prompt

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,5 @@
+import { createInterface } from 'node:readline';
+
 export function cleanInput(input: string): string[] {
     const textList = input.toLowerCase().split(" ");
     const result: string[] = [];
@@ -8,4 +10,32 @@ export function cleanInput(input: string): string[] {
         result.push(word)
     }
     return result
+}
+
+export function startREPL() {
+    // Create the readline interface
+    const rl = createInterface({ 
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Pokedex > '
+    });
+
+    // Display the initial prompt
+    rl.prompt();
+
+    // Listen for line input
+    rl.on('line', (line:string) => {
+        const words = cleanInput(line);
+
+        if (words.length === 0) {
+            // If input is empty, re-prompt and exit callback
+            rl.prompt();
+            return;
+        }
+
+        console.log(`Your command was: ${words[0]}`);
+
+        // Re-prompt to allow for another command
+        rl.prompt();
+    });
 }
